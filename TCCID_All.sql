fb32ed9b665b4c14a40e9f799b98295b5bad0b50
Inigo San Gil <isangil@lternet.edu>
2020-04-23 13:56:26 -0600

    Adds small extract query for syn'ing ORIEN identifiers

---
+++ TCCID_All.sql
@@ -0,0 +1,10 @@
+SELECT
+    ER_PATPROT.PATPROT_PATSTDID AS TCCID    -- Patient Study ID [TCCID]
+    ,PERSON.PERSON_DOB AS DOB                -- Patient Date of Birth [DOB]
+    ,PERSON.PERSON_CODE AS MRN               -- Patient ID [MRN]
+FROM  ERES.ER_PATPROT ER_PATPROT 
+      INNER JOIN ERES.ER_PER ER_PER     ON ER_PATPROT.FK_PER = ER_PER.PK_PER
+      INNER JOIN EPAT.PERSON PERSON     ON ER_PER.PER_CODE = PERSON.PERSON_CODE
+WHERE
+    ER_patprot.fk_study = 4168  -- this is the prim. key for the ORIEN study
+    AND ER_PATPROT.PATPROT_ENROLDT >= TO_DATE('01-01-2016', 'DD-MM-YYYY') 
