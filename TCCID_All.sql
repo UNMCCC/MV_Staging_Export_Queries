4bb854b2be55e89ef6c1224a508b4843cdee1e8b
Inigo San Gil <isangil@lternet.edu>
2020-04-23 14:09:48 -0600

    Wraps the tccid query around the commands to export as CSV

--- TCCID_All.sql
+++ TCCID_All.sql
@@ -1,3 +1,14 @@
+SET PAGES 0
+SET FEEDBACK OFF
+set echo off
+set linesize 6000
+set pagesize 0
+set sqlprompt ''
+set trimspool on
+SET TERMOUT OFF
+set headsep off
+spool D:\MiniVelos\Sources\tccids.csv
+set markup csv on
 SELECT
     ER_PATPROT.PATPROT_PATSTDID AS TCCID    -- Patient Study ID [TCCID]
     ,PERSON.PERSON_DOB AS DOB                -- Patient Date of Birth [DOB]
@@ -7,4 +18,6 @@ FROM  ERES.ER_PATPROT ER_PATPROT
       INNER JOIN EPAT.PERSON PERSON     ON ER_PER.PER_CODE = PERSON.PERSON_CODE
 WHERE
     ER_patprot.fk_study = 4168  -- this is the prim. key for the ORIEN study
-    AND ER_PATPROT.PATPROT_ENROLDT >= TO_DATE('01-01-2016', 'DD-MM-YYYY') 
+    AND ER_PATPROT.PATPROT_ENROLDT >= TO_DATE('01-01-2016', 'DD-MM-YYYY') ;
+spool off;
+exit;
